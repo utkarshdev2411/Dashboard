6607f8d3c07b49ada1023b28a0996e6aff332040
Utkarsh Sharma <utkarsh.cr@coolbootsmedia.co>
2025-07-15 01:58:19 +0530

    chore: initialize frontend with Next.js and Tailwind CSS setup
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Included essential libraries such as Radix UI components, Axios, and Recharts.
    - Set up PostCSS configuration for Tailwind CSS integration.

---
+++ frontend/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: [
+      'dribbble.com',
+      'res.cloudinary.com',
+      'cdn.dummyjson.com',
+      'www.pngmart.com', 
+    ],
+  },
+};
+
+export default nextConfig;
