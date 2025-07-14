58b15e6fbd9ad9a5e211f7d5aacd2a5d190506f3
Utkarsh Sharma <utkarsh.cr@coolbootsmedia.co>
2025-07-15 02:34:56 +0530

    chore: update Next.js configuration and add export script to package.json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: [
       'dribbble.com',
       'res.cloudinary.com',
